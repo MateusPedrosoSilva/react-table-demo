9c6b58a8322ce7c9f9288ab4c053f79fe2f7e74a
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2021-04-05 01:25:51 -0300

    mounted a basic table

---
+++ src/components/columns.js
@@ -0,0 +1,26 @@
+export const COLUMNS = [
+    {
+        Header: 'id',
+        Accessor: 'id',
+    },
+    {
+        Header: 'First name',
+        Accessor: 'first_name',
+    },
+    {
+        Header: 'Last name',
+        Accessor: 'last_name',
+    },
+    {
+        Header: 'Date of birth',
+        Accessor: 'date_of_birth',
+    },
+    {
+        Header: 'Country',
+        Accessor: 'country',
+    },
+    {
+        Header: 'Phone',
+        Accessor: 'phone',
+    },
+];
