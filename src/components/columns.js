5e15a438e59bf8f68f6c2f8e53a0cba2fdf628a2
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2021-04-05 10:54:38 -0300

    Make some use of Header groups, just for test

--- src/components/columns.js
+++ src/components/columns.js
@@ -30,3 +30,48 @@ export const COLUMNS = [
     accessor: 'phone',
   },
 ];
+
+export const GROUPED_COLUMNS = [
+  {
+    Header: 'id',
+    Footer: 'id',
+    accessor: 'id',
+  },
+  {
+    Header: 'Name',
+    Footer: 'Name',
+    columns: [
+      {
+        Header: 'First name',
+        Footer: 'First name',
+        accessor: 'first_name',
+      },
+      {
+        Header: 'Last name',
+        Footer: 'Last name',
+        accessor: 'last_name',
+      },
+    ]
+  },
+  {
+    Header: 'Info',
+    Footer: 'Info',
+    columns: [
+      {
+        Header: 'Date of birth',
+        Footer: 'Date of birth',
+        accessor: 'date_of_birth',
+      },
+      {
+        Header: 'Country',
+        Footer: 'Country',
+        accessor: 'country',
+      },
+      {
+        Header: 'Phone',
+        Footer: 'Phone',
+        accessor: 'phone',
+      },
+    ]
+  },
+];
