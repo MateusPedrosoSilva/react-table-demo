57c296a2b430986d8ce7e39f59aaae18fe88d785
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2021-04-09 14:39:56 -0300

    Atualizada as colunas das tabelas

--- src/components/columns.js
+++ src/components/columns.js
@@ -2,34 +2,144 @@
 
 export const COLUMNS = [
   {
-    Header: 'Loja Pedido',
-    Footer: 'Loja Pedido',
-    accessor: 'LOJAPEDIDO',
+    Header: 'Prioridade',
+    Footer: 'Prioridade',
+    accessor: 'PRIORIDADE',
   },
   {
-    Header: 'Loja Origem',
-    Footer: 'Loja Origem',
-    accessor: 'LOJAORIGEM',
+    Header: 'Placa',
+    Footer: 'Placa',
+    accessor: 'PLACA',
   },
   {
-    Header: 'Pedido',
-    Footer: 'Pedido',
-    accessor: 'PEDIDO',
+    Header: 'Segunda placa',
+    Footer: 'Segunda placa',
+    accessor: 'SEGUNDA_PLACA',
   },
   {
-    Header: 'Carga',
-    Footer: 'Carga',
-    accessor: 'CARGA',
+    Header: 'Motorista CPF',
+    Footer: 'Motorista CPF',
+    accessor: 'MOTORISTA_CPF',
   },
   {
-    Header: 'Produto',
-    Footer: 'Produto',
-    accessor: 'PRODUTO',
+    Header: 'Referência de rota',
+    Footer: 'Referência de rota',
+    accessor: 'REFERENCIA_DE_ROTA',
   },
   {
-    Header: 'Descrição',
-    Footer: 'Descrição',
-    accessor: 'DESCRICAO',
+    Header: 'Data início rota',
+    Footer: 'Data início rota',
+    accessor: 'DATA_INICIO_ROTA',
+  },
+  {
+    Header: 'Data previsão de atendimento',
+    Footer: 'Data previsão de atendimento',
+    accessor: 'DATA_PREVISAO_ATENDIMENTO',
+  },
+  {
+    Header: 'Sequência',
+    Footer: 'Sequência',
+    accessor: 'SEQUENCIA',
+  },
+  {
+    Header: 'Retorna origem',
+    Footer: 'Retorna origem',
+    accessor: 'RETORNA_ORIGEM',
+  },
+  {
+    Header: 'Cód. destino CPF',
+    Footer: 'Cód. destino CPF',
+    accessor: 'COD_DESTINO_CPF',
+  },
+  {
+    Header: 'Cód. embarcador',
+    Footer: 'Cód. embarcador',
+    accessor: 'COD_EMBARCADOR',
+  },
+  {
+    Header: 'Descrição do destino',
+    Footer: 'Descrição do destino',
+    accessor: 'DESCRICAO_DESTINO',
+  },
+  {
+    Header: 'Telefone',
+    Footer: 'Telefone',
+    accessor: 'TELEFONE',
+  },
+  {
+    Header: 'Coleta entrega serviço',
+    Footer: 'Coleta entrega serviço',
+    accessor: 'COLETA_ENTREGA_SERVICO',
+  },
+  {
+    Header: 'Documento',
+    Footer: 'Documento',
+    accessor: 'DOCUMENTO',
+  },
+  {
+    Header: 'Tipo documento',
+    Footer: 'Tipo documento',
+    accessor: 'TIPO_DOCUMENTO',
+  },
+  {
+    Header: 'Chave de acesso',
+    Footer: 'Chave de acesso',
+    accessor: 'CHAVE_ACESSO',
+  },
+  {
+    Header: 'Cód. postal',
+    Footer: 'Cód. postal',
+    accessor: 'CODIGO_POSTAL',
+  },
+  {
+    Header: 'Logradouro',
+    Footer: 'Logradouro',
+    accessor: 'LOGRADOURO',
+  },
+  {
+    Header: 'Número',
+    Footer: 'Número',
+    accessor: 'NUMERO',
+  },
+  {
+    Header: 'Complemento',
+    Footer: 'Complemento',
+    accessor: 'COMPLEMENTO',
+  },
+  {
+    Header: 'Bairro',
+    Footer: 'Bairro',
+    accessor: 'BAIRRO',
+  },
+  {
+    Header: 'Município',
+    Footer: 'Município',
+    accessor: 'MUNICIPIO',
+  },
+  {
+    Header: 'Estado',
+    Footer: 'Estado',
+    accessor: 'ESTADO',
+  },
+  {
+    Header: 'País',
+    Footer: 'País',
+    accessor: 'PAIS',
+  },
+  {
+    Header: 'Latitude',
+    Footer: 'Latitude',
+    accessor: 'LATITUDE',
+  },
+  {
+    Header: 'Longitude',
+    Footer: 'Longitude',
+    accessor: 'LONGITUDE',
+  },
+  {
+    Header: 'Peso',
+    Footer: 'Peso',
+    accessor: 'PESO',
   },
   {
     Header: 'Quantidade',
@@ -37,19 +147,49 @@ export const COLUMNS = [
     accessor: 'QUANTIDADE',
   },
   {
-    Header: 'Doca',
-    Footer: 'Doca',
-    accessor: 'DOCA',
+    Header: 'Volume',
+    Footer: 'Volume',
+    accessor: 'VOLUME',
   },
   {
-    Header: 'Veículo',
-    Footer: 'Veículo',
-    accessor: 'VEICULO',
+    Header: 'Valor mercadoria',
+    Footer: 'Valor mercadoria',
+    accessor: 'VALOR_MERCADORIA',
   },
   {
-    Header: 'Data Pedido',
-    Footer: 'Data Pedido',
-    accessor: 'DATAPEDIDO',
+    Header: 'Prazo de entrega',
+    Footer: 'Prazo de entrega',
+    accessor: 'PRAZO_ENTREGA',
+  },
+  {
+    Header: 'Data agendamento',
+    Footer: 'Data agendamento',
+    accessor: 'DATA_AGENDAMENTO',
+  },
+  {
+    Header: 'Tempo de atendimento',
+    Footer: 'Tempo de atendimento',
+    accessor: 'TEMPO_ATENDIMENTO',
+  },
+  {
+    Header: 'Horário início da janala',
+    Footer: 'Horário início da janala',
+    accessor: 'HORARIO_INICIO_JANELA',
+  },
+  {
+    Header: 'Cód. tipo carga',
+    Footer: 'Cód. tipo carga',
+    accessor: 'COD_TIPO_CARGA',
+  },
+  {
+    Header: 'Cód. operação',
+    Footer: 'Cód. operação',
+    accessor: 'COD_OPERACAO',
+  },
+  {
+    Header: 'Observações',
+    Footer: 'Observações',
+    accessor: 'OBSERVACOES',
   },
 ];
 
