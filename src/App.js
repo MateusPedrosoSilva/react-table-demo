a6db876776deaeeb366f7fa0bb315943ddd48b70
Patrick Porto <patrickporto.dev@gmail.com>
2021-04-22 08:20:57 -0300

    modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import './App.css';
 import { Roterizador } from './components/Roterizador';
 
-// import pdfMake from "pdfmake/build/pdfmake";
-// import pdfFonts from "pdfmake/build/vfs_fonts";
+import Modal from 'react-modal';
 
-// import { Impressao } from './impressao';
+import React, {useState} from 'react';
 
 // import { RowSelection } from './components/RowselectionTable';
 // import { FilteringTable } from './components/FilteringTable';
 // import { PaginationTable } from './components/PaginationTable';
 // import { BasicTable } from './components/BasicTable';
 // import { SortingTable } from './components/SortingTable';
-
+Modal.setAppElement('#root');
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
   return (
     // <div className="App">
     //   <RowSelection />
     // </div>
     <div>
+      <button onClick={()=> setModalIsOpen(true)}>Visualizar envio</button>
+      <Modal isOpen={modalIsOpen} shouldCloseOnOverlayClick={false} onRequestClose={()=> setModalIsOpen(false)}>
+
+    <button onClick={()=> setModalIsOpen(false)}>Fechar
+    </button>
+      </Modal>
       <Roterizador />
     </div>
   );
